77ed0048cf8fbabbf24771c29521d4e84e7a3850
Mario Borna Mjertan <bor.mbm@gmail.com>
2013-02-16 16:07:15 +0100

    Update _class/db-conf.php

--- _class/db-conf.php
+++ _class/db-conf.php
@@ -1,12 +1,12 @@
 <?php
 $obj = new modernCMS();
 
-// Postavlja vezu s databazom - varijable veze
+// Sets the database connection variables
 $obj->host = 'localhost'; //change this
 $obj->username = 'root'; //and this
 $obj->password = 'hamster'; // this, as well
 $obj->db = 'minima'; // aaaand this
 
-// Spajanje na databazu
+// connects to the database
 $obj->connect();
 ?>
